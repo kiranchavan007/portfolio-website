d224080b38d17d0534fd829ead9c320b7bf0e235
Kiran Chavan <kiranmchavan018@gmail.com>
2025-02-27 19:04:01 +0530

    last time

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6dd4adb9a0c2f8bd8b7112e0cb6800d7",
+"flutter_bootstrap.js": "d7ac3d2b2c1225f6d510e0f1b0fff59f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
